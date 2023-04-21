4b1a111e8a46ca4c51b9bfe10355b15009727b3f
enochsori <enoch.ha915@gmail.com>
2023-04-21 13:03:38 -0400

    done

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const app = Vue.createApp({
   data() {
     return {
-      inputClass: "",
+      inputClass: '',
+      confirmedClassName: '',
+      inputColor: '',
+      confirmedBGColor: '',
+      isUser1Selected: false,
+      isUser2Selected: false,
+      isVisibleSelected: true,
+      isHiddenSelected: false,
     };
   },
-  computed: {
-    handleAddClassName() {},
+  methods: {
+    handleAddClassName() {
+      if (this.inputClass === 'user1') {
+        this.user1 = true;
+        this.user2 = false;
+      } else if (this.inputClass === 'user2') {
+        this.user1 = false;
+        this.user2 = true;
+      }
+
+      this.confirmedClassName = {
+        user1: this.user1,
+        user2: this.user2,
+      };
+    },
+
+    handleToggleVisibility() {
+      this.isHiddenSelected = !this.isHiddenSelected;
+      this.isVisibleSelected = !this.isVisibleSelected;
+
+      this.confirmedClassName = {
+        ...this.confirmedClassName,
+        visible: this.isVisibleSelected,
+        hidden: this.isHiddenSelected,
+      };
+    },
+    handleAddBackgroundColor() {
+      this.confirmedBGColor = this.inputColor;
+    },
   },
 });
 
-app.mount("#assignment");
+app.mount('#assignment');
